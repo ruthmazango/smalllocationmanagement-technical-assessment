653397cb2c48f0aa51ebb4a1a414f2c74ea90759
ruramisai <ruthmazango@gmail.com>
2023-07-05 14:35:01 +0200

    Initial commit

---
+++ src/main/java/com/system/smalllocationmanagement/SmalllocationmanagementApplication.java
@@ -0,0 +1,15 @@
+package com.system.smalllocationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class SmalllocationmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmalllocationmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/smalllocationmanagement/SmalllocationmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.smalllocationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmalllocationmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
